64bcd53dda8cb5a09b8be8ef37cf07a838f8c285
Daniela <dgcastiblanco02@gmail.com>
2021-08-10 09:08:11 -0500

    Agregar archivos de proyecto.

---
+++ framework.asp/Controllers/UsuarioController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using framework.asp;
+
+namespace framework.asp.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        // GET: Usuario
+        public ActionResult Index()
+        {
+            using (var db = new inventario2021())
+            {
+                return View();
+            }
+            
+        }
+    }
+}
---
+++ framework.asp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="framework.asp.MvcApplication" Language="C#" %>
